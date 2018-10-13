557b7b4d4b34c815a227effa159348d3240e03d9
becc-mu <becc.mulugeta@gmail.com>
2018-10-13 13:28:20 +0100

    added print menu

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -10,8 +10,13 @@ describe Menu do
       chips: 2.90
     }
   end
-  
+
   it 'has a list of dishes with prices' do
     expect(menu.dishes).to eq(dishes)
+  end
+
+  it 'prints a list of dishes with prices' do
+    printed_menu = "Soup £3.45, Fish £6.90, Chips £2.90"
+    expect(menu.print).to eq(printed_menu)
   end
 end
